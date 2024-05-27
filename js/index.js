b19dfa7dbafe808dc9ebbebb248fe0b24b0038be
joaquin pereira <joaquinpereirapiriz@gmail.com>
2024-05-27 13:39:17 -0300

    cambiando la cantidad de equipos a enfrentar

--- js/index.js
+++ js/index.js
@@ -1,11 +1,11 @@
 alert(
-  "🏆⚽ BIENVENIDO AL TORNEO DE PENALES CONTRA LOS 16 MEJORES EQUIPOS DE LA CHAMPIONS LEAGUE ⚽🏆. Podrá ver el historial de partidos en la consola(aprete F12)"
+  "🏆⚽ BIENVENIDO AL TORNEO DE PENALES CONTRA LOS 8 MEJORES EQUIPOS DE LA CHAMPIONS LEAGUE ⚽🏆. \nPodrá ver el historial de partidos en la consola(aprete F12)"
 );
 
 // Variables globales
 let nombreUsuario = prompt("Ingrese su nombre").toLowerCase();
 let puntos = 0;
-let totalEquipos = 18;
+let totalEquipos = 8;
 let partidosJugados = 0;
 
 // Variables para verificar que no juegue mas de una vez contra el mismo equipo
@@ -17,16 +17,6 @@ let jugado4 = false;
 let jugado5 = false;
 let jugado6 = false;
 let jugado7 = false;
-let jugado8 = false;
-let jugado9 = false;
-let jugado10 = false;
-let jugado11 = false;
-let jugado12 = false;
-let jugado13 = false;
-let jugado14 = false;
-let jugado15 = false;
-let jugado16 = false;
-let jugado17 = false;
 
 function seleccionarEquipo() {
   let sorteoRival;
@@ -40,15 +30,7 @@ function seleccionarEquipo() {
     (sorteoRival === 4 && jugado4) ||
     (sorteoRival === 5 && jugado5) ||
     (sorteoRival === 6 && jugado6) ||
-    (sorteoRival === 7 && jugado7) ||
-    (sorteoRival === 8 && jugado8) ||
-    (sorteoRival === 9 && jugado9) ||
-    (sorteoRival === 10 && jugado10) ||
-    (sorteoRival === 11 && jugado11) ||
-    (sorteoRival === 12 && jugado12) ||
-    (sorteoRival === 13 && jugado13) ||
-    (sorteoRival === 14 && jugado14) ||
-    (sorteoRival === 15 && jugado15)
+    (sorteoRival === 7 && jugado7)
   );
   return sorteoRival;
 }
@@ -80,8 +62,8 @@ while (partidosJugados < totalEquipos) {
       jugado2 = true;
       break;
     case 3:
-      equipoRival = "FC Copenhague";
-      arqueroRival = "Grabara";
+      equipoRival = "PSG";
+      arqueroRival = "Donnarumma";
       jugado3 = true;
       break;
     case 4:
@@ -90,8 +72,8 @@ while (partidosJugados < totalEquipos) {
       jugado4 = true;
       break;
     case 5:
-      equipoRival = "Lazio";
-      arqueroRival = "Provedel";
+      equipoRival = "Arsenal";
+      arqueroRival = "Raya";
       jugado5 = true;
       break;
     case 6:
@@ -100,49 +82,9 @@ while (partidosJugados < totalEquipos) {
       jugado6 = true;
       break;
     case 7:
-      equipoRival = "PSV";
-      arqueroRival = "Benitez";
-      jugado7 = true;
-      break;
-    case 8:
-      equipoRival = "Napoli";
-      arqueroRival = "Meret";
-      jugado8 = true;
-      break;
-    case 9:
-      equipoRival = "Real Sociedad";
-      arqueroRival = "Remiro";
-      jugado9 = true;
-      break;
-    case 10:
-      equipoRival = "Porto";
-      arqueroRival = "Costa";
-      jugado10 = true;
-      break;
-    case 11:
       equipoRival = "Dortmund";
       arqueroRival = "Kobel";
-      jugado11 = true;
-      break;
-    case 12:
-      equipoRival = "Arsenal";
-      arqueroRival = "Raya";
-      jugado12 = true;
-      break;
-    case 13:
-      equipoRival = "Inter";
-      arqueroRival = "Onana";
-      jugado13 = true;
-      break;
-    case 14:
-      equipoRival = "PSG";
-      arqueroRival = "Donnarumma";
-      jugado14 = true;
-      break;
-    case 15:
-      equipoRival = "Leipzig";
-      arqueroRival = "Gulacsi";
-      jugado15 = true;
+      jugado7 = true;
       break;
     default:
       equipoRival = "";
@@ -163,7 +105,11 @@ while (partidosJugados < totalEquipos) {
   function patearUsuario() {
     let direccionPatear = prompt(
       nombreUsuario +
-        " TE TOCA PATEAR! A donde quieres patear? 'i' = Izquierda, 'm' = Medio, 'd' = Derecha. Recuerda que el que ataja es " +
+        " TE TOCA PATEAR! A donde quieres patear?\n" +
+        "'i' = Izquierda,\n" +
+        "'m' = Medio,\n" +
+        "'d' = Derecha.\n" +
+        "Recuerda que el que ataja es " +
         arqueroRival
     ).toLowerCase();
 
@@ -213,37 +159,37 @@ while (partidosJugados < totalEquipos) {
       case "Gol a la derecha":
         goles++;
         alert(
-          `¡QUE GOLAZOOOOOOOOOOOOO! Resultado actual: ${nombreUsuario}: ${goles} - ${equipoRival}: ${atajadas}`
+          `¡QUE GOLAZOOOOOOOOOOOOO! \n Resultado actual: ${nombreUsuario}: ${goles} - ${equipoRival}: ${atajadas}`
         );
         console.log(
-          `¡QUE GOLAZOOOOOOOOOOOOO! Resultado actual: ${nombreUsuario}: ${goles} - ${equipoRival}: ${atajadas}`
+          `¡QUE GOLAZOOOOOOOOOOOOO! \n Resultado actual: ${nombreUsuario}: ${goles} - ${equipoRival}: ${atajadas}`
         );
         break;
       case "atajada":
         atajadas++;
         alert(
-          `¡Atajó el arquero rival! Resultado actual: ${nombreUsuario}: ${goles}  - ${equipoRival}: ${atajadas}`
+          `¡Atajó el arquero rival! \n Resultado actual: ${nombreUsuario}: ${goles}  - ${equipoRival}: ${atajadas}`
         );
         console.log(
-          `¡Atajó el arquero rival! Resultado actual: ${nombreUsuario}: ${goles}  - ${equipoRival}: ${atajadas}`
+          `¡Atajó el arquero rival! \n Resultado actual: ${nombreUsuario}: ${goles}  - ${equipoRival}: ${atajadas}`
         );
         break;
       case "afuera":
         atajadas++;
         alert(
-          `¡LA MANDASTE A LA CHINA! Resultado actual: ${nombreUsuario}: ${goles}  - ${equipoRival}: ${atajadas}`
+          `¡LA MANDASTE A LA CHINA! \n Resultado actual: ${nombreUsuario}: ${goles}  - ${equipoRival}: ${atajadas}`
         );
         console.log(
-          `¡LA MANDASTE A LA CHINA! Resultado actual: ${nombreUsuario}: ${goles}  - ${equipoRival}: ${atajadas}`
+          `¡LA MANDASTE A LA CHINA! \n Resultado actual: ${nombreUsuario}: ${goles}  - ${equipoRival}: ${atajadas}`
         );
         break;
       case "palo":
         atajadas++;
         alert(
-          `¡AAAAAL PAAALOO! Resultado actual: ${nombreUsuario}: ${goles}  - ${equipoRival}: ${atajadas}`
+          `¡AAAAAL PAAALOO! \n Resultado actual: ${nombreUsuario}: ${goles}  - ${equipoRival}: ${atajadas}`
         );
         console.log(
-          `¡AAAAAL PAAALOO! Resultado actual: ${nombreUsuario}: ${goles}  - ${equipoRival}: ${atajadas}`
+          `¡AAAAAL PAAALOO! \n Resultado actual: ${nombreUsuario}: ${goles}  - ${equipoRival}: ${atajadas}`
         );
         break;
       default:
@@ -254,18 +200,18 @@ while (partidosJugados < totalEquipos) {
 
   if (goles < atajadas) {
     alert(
-      `FINAL DEL PARTIDO!! GANÓ: ${equipoRival}. Por ${atajadas} a ${goles} 😭😭😭 . ${nombreUsuario} tienes ${puntos} puntos.`
+      `FINAL DEL PARTIDO!! \n GANÓ: ${equipoRival}. \n Por ${atajadas} a ${goles} 😭😭😭 . \n ${nombreUsuario} tienes ${puntos} puntos.`
     );
     console.log(
-      `FINAL DEL PARTIDO!! GANÓ: ${equipoRival}. Por ${atajadas} a ${goles} 😭😭😭 . ${nombreUsuario} tienes ${puntos} puntos.`
+      `FINAL DEL PARTIDO!! \n GANÓ: ${equipoRival}. \n Por ${atajadas} a ${goles} 😭😭😭 . \n ${nombreUsuario} tienes ${puntos} puntos.`
     );
   } else {
     puntos += 3;
     alert(
-      `FINAL DEL PARTIDO!! GANÓ: ${nombreUsuario}. Por ${goles} a ${atajadas} 😎😎😎. Tienes ${puntos} puntos.`
+      `FINAL DEL PARTIDO!! \n GANÓ: ${nombreUsuario}. \n Por ${goles} a ${atajadas} 😎😎😎. \n Tienes ${puntos} puntos.`
     );
     console.log(
-      `FINAL DEL PARTIDO!! GANÓ: ${nombreUsuario}. Por ${goles} a ${atajadas} 😎😎😎. Tienes ${puntos} puntos.`
+      `FINAL DEL PARTIDO!! \n GANÓ: ${nombreUsuario}. \n Por ${goles} a ${atajadas} 😎😎😎. \n Tienes ${puntos} puntos.`
     );
   }
 
@@ -273,9 +219,9 @@ while (partidosJugados < totalEquipos) {
 }
 
 alert(
-  `El torneo ha terminado⚽⚽. ${nombreUsuario} has conseguido ${puntos} puntos 🏆🏆.`
+  `El torneo ha terminado⚽⚽. \n ${nombreUsuario} has conseguido ${puntos} puntos 🏆🏆.`
 );
 
 console.log(
-  `El torneo ha terminado⚽⚽. ${nombreUsuario} has conseguido ${puntos} puntos 🏆🏆.`
+  `El torneo ha terminado⚽⚽. \n ${nombreUsuario} has conseguido ${puntos} puntos 🏆🏆.`
 );
